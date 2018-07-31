15c08f9a6ec4178a7a0428e248066b2cd1c69924
RajdeepKonwar <rkonwar@ucsd.edu>
2018-07-30 20:07:32 -0700

    Added Gmsh binary and minor changes

--- gen_mesh.sh
+++ gen_mesh.sh
@@ -3,88 +3,94 @@
 # Usage info
 function show_help() {
 cat << EOF
-Usage: ${0##*/} [-h] [-d] [-f GEOCONF -g OUTGEO -m OUTMSH -o EUREKADAT]
+Usage: ${0##*/} [-h] [-c CONF -g GEO -m MSH -d DAT] [-x]
 Generates Eureka format mesh file (.dat) using Gmsh as meshing-tool.
-    -h           This help message.
-    -d           Do not delete intermediate mat file (optional).
-    -f GEOCONF   GeoGen input config file.
-    -g OUTGEO    GeoGen output geo file.
-    -m OUTMSH    Gmsh output msh file.
-    -o EUREKADAT EurekGen output mesh file.
+    -h      This help message.
+    -c CONF GeoGen input config file.
+    -g GEO  GeoGen output geo file.
+    -m MSH  Gmsh output msh file.
+    -d DAT  EurekGen output mesh file.
+    -x      Do not delete intermediate mat file (optional).
 EOF
 }
 
 # Parse command line arguments
 OPTIND=1
-DELETEMAT=1
-GEOCONF=""
-OUTGEO=""
-OUTMSH=""
-EUREKADAT=""
+MAT=1
+#CONF=""
+#GEO=""
+#MSH=""
+#DAT=""
 
-while getopts "hdf:g:m:o:" opt; do
+while getopts "hc:g:m:d:x" opt; do
   case "$opt" in
     h)
-      show_help                         # Show help
-      return
+      show_help       # Show help
+      exit 0
       ;;
-    d)
-      DELETEMAT=0                       # Do not delete intermediate mat file
-      ;;
-    f)
-      GEOCONF=$OPTARG                   # GeoGen input config file
+    c)
+      CONF=$OPTARG    # GeoGen input config file
       ;;
     g)
-      OUTGEO=$OPTARG                    # GeoGen output geo file
+      GEO=$OPTARG     # GeoGen output geo file
       ;;
     m)
-      OUTMSH=$OPTARG                    # Gmsh output msh file
+      MSH=$OPTARG     # Gmsh output msh file
       ;;
-    o)
-      EUREKADAT=$OPTARG                 # EurekGen output mesh file
+    d)
+      DAT=$OPTARG     # EurekGen output mesh file
+      ;;
+    x)
+      MAT=0           # Do not delete intermediate mat file (optional)
       ;;
     \?)
       show_help >&2
-      return
+      exit 1
       ;;
     esac
 done
 shift "$((OPTIND-1))" # Shift off the options and optional --.
 
 # Check command line arguments
-if [[ ( -z "$GEOCONF" || -z "$OUTGEO" || -z "$OUTMSH" || -z "$EUREKADAT" ) ]]
+if [[ ( -z "$CONF" || -z "$GEO" || -z "$MSH" || -z "$DAT" ) ]]
 then
   show_help >&2
-  return
+  exit 1
 fi
 
+# Check if gmsh binary is present
+command -v ./gmsh >/dev/null 2>&1 || { echo >&2 "Gmsh binary needs to be in MeshGen directory! Exiting.."; exit 1; }
+
+# Check if GeoGen and EurekaGen are built
+command -v ./GeoGen/GeoGen >/dev/null 2>&1 || { echo >&2 "GeoGen needs to be built first! Type 'make'.."; exit 1; }
+command -v ./EurekaGen/EurekaGen >/dev/null 2>&1 || { echo >&2 "EurekaGen needs to be built first! Type 'make'.."; exit 1; }
 
 # Create mesh dir (if missing)
 mkdir -p mesh
 
 # Generate geo using GeoGen program
-printf "\n---------------------"
-printf "\nGenerating geo file.."
-printf "\n---------------------\n"
-./GeoGen/GeoGen -f $GEOCONF -o $OUTGEO
+printf "\n-----------------------------"
+printf "\nGeoGen: Generating geo file.."
+printf "\n-----------------------------\n"
+./GeoGen/GeoGen -f $CONF -o $GEO
 
 SECONDS=0
 # Generate msh using Gmsh program
-printf "\n---------------------"
-printf "\nGenerating msh file.."
-printf "\n---------------------\n"
-gmsh $OUTGEO -3 -o $OUTMSH
+printf "\n---------------------------"
+printf "\nGmsh: Generating msh file.."
+printf "\n---------------------------\n"
+./gmsh $GEO -3 -o $MSH
 duration=$SECONDS
 printf "Time taken = $duration s\n"
 
 # Generate dat using EurekaGen program
-printf "\n---------------------"
-printf "\nGenerating dat file.."
-printf "\n---------------------\n"
-./EurekaGen/EurekaGen -f $GEOCONF -i $OUTMSH -o $EUREKADAT
+printf "\n--------------------------------"
+printf "\nEurekaGen: Generating dat file.."
+printf "\n--------------------------------\n"
+./EurekaGen/EurekaGen -f $CONF -i $MSH -o $DAT
 
 # Remove intermediate material file
-if [ $DELETEMAT -eq 1 ]
+if [ $MAT -eq 1 ]
 then
   rm -f GeoGen.mat
 fi
